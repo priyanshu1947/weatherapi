1ab1960c7c8468392fd50d080806ec93a7276bc2
priyanshu1947 <70458921+priyanshu1947@users.noreply.github.com>
2024-02-20 02:03:20 +0530

    first commit

---
+++ weatherApp-Reactjs-master/src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "6c15eaab9a82578e643292a8c1bc8651",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
